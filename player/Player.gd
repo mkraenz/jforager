a8b65e0411f503d3105fe69cb808ef0c68d273be
Mirco Kraenz <software@kraenz.eu>
2023-07-23 16:00:44 +0200

    init

---
+++ player/Player.gd
@@ -0,0 +1,58 @@
+extends CharacterBody2D
+
+
+const SPEED := 100.0
+const HORIZONTAL_SPEED := 50.0
+const MAX_VERTICAL_SPEED := 1000.0
+const BRAKE_STRENGTH := 3.0
+const PUSH_STRENGTH := 50.0
+const acceleration_cooldown := 0.7
+const min_left :=15
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var anims: AnimatedSprite2D = $AnimSprite
+@onready var pushPoleAnimTimer: Timer = $AnimSprite/PushPoleTimer
+
+var acceleration_cooldown_timer := acceleration_cooldown + 1.0 # start with cooldown expired
+var apply_delayed_acceleration = false
+
+func _physics_process(delta):
+	acceleration_cooldown_timer += delta
+
+	# add gravity to simulate sliding down the slope.
+	velocity.y += gravity * delta
+
+	# if breaking, slow down the vertical speed.
+	if Input.is_action_pressed("brake"):
+		velocity.y = move_toward(velocity.y, 0, BRAKE_STRENGTH)
+		
+	if apply_delayed_acceleration:
+		velocity.y = move_toward(velocity.y, MAX_VERTICAL_SPEED, PUSH_STRENGTH)
+		apply_delayed_acceleration = false
+
+	if Input.is_action_just_pressed("accelerate") and acceleration_cooldown_timer > acceleration_cooldown:
+		acceleration_cooldown_timer = 0.0
+		pushPoleAnimTimer.start()
+		anims.play('push_pole')
+
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * HORIZONTAL_SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, HORIZONTAL_SPEED)
+
+	if position.x<min_left and velocity.x <0:
+		velocity.x = 0
+
+	if position.x>310 and velocity.x >0:
+		velocity.x = 0
+
+	move_and_slide()
+
+
+
+
+func _on_push_pole_timer_timeout():
+	anims.play('default')
+	apply_delayed_acceleration = true
---
+++ ui/TotalTime.gd
@@ -0,0 +1,32 @@
+extends Label
+
+@onready var eventbus := Eventbus
+
+var time_in_secs := 0.0
+var final_time_in_secs := -1.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time_in_secs += delta
+	update_text(time_in_secs)
+	
+func update_text(given_time: float) -> void:
+	var time = snappedf(given_time, 0.001)
+	
+	var millis = fmod(time * 1000, 1000)
+	var seconds = fmod(time, 60)
+	var minutes = fmod(time / 60, 60)
+	# format "MM:SS:MMM"
+	text = "%02d:%02d:%03d" % [minutes, seconds, millis]
+
+func _on_goal_body_entered(_body) -> void:
+	print("RECEIVED GOAL AREA ENTERED")
+	final_time_in_secs = time_in_secs
+	update_text(final_time_in_secs)
+	set_process(false)
+
+func _ready():
+	eventbus.connect('goal_reached', _on_goal_reached)
+
+func _on_goal_reached():
+	set_process(false)
