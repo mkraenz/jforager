f11f1576caa41ae7c396d034633c381dcf8cef35
Mirco Kraenz <software@kraenz.eu>
2023-07-23 19:59:08 +0200

    feat: edit movement

--- player/Player.gd
+++ player/Player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
-const SPEED := 100.0
+@export var SPEED := 100.0  # speed in pixels/sec
 
 @onready var anims: AnimatedSprite2D = $AnimSprite
 
 
 func _physics_process(delta):
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * SPEED
+
 	move_and_slide()
