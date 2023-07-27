40065d0b7b8f7780ce8b9691c6d37e619809d1c0
Mirco Kraenz <software@kraenz.eu>
2023-07-27 15:14:55 +0200

    feat: select stone

---
+++ world/resources/Stone.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+
+
+# https://padamthapa.com/blog/how-to-detect-click-inside-staticbody2d-in-godot/
+func _input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print("A good Click!")
