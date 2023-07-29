aad8e3cbaa891ad8e7d2fa53049db37b2c1e0f66
Mirco Kraenz <software@kraenz.eu>
2023-07-29 11:54:22 +0200

    feat: respawning resources

--- World.gd
+++ World.gd
@@ -3,7 +3,14 @@ extends Node2D
 var scene = preload("res://world/resources/Stone.tscn")
 
 
-func _ready():
+func _on_resource_respawn_timer_timeout():
+	print("hello world")
+	var rng = RandomNumberGenerator.new()
+	var my_random_x = rng.randf_range(0.00, 325.0)
+	var my_random_y = rng.randf_range(0.0, 1300.0)
 	var stone = scene.instantiate()
 	add_child(stone)
-	stone.global_position = Vector2(100, 100)
+	stone.global_position = Vector2(my_random_x, my_random_y)
+
+	print(my_random_x)
+	print(my_random_y)
