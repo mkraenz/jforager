a3c4850ee9910f162a3b2f4ee8b3a0060e3e656a
Mirco Kraenz <software@kraenz.eu>
2023-07-29 15:47:14 +0200

    cleanup

--- World.gd
+++ World.gd
@@ -4,13 +4,9 @@ var scene = preload("res://world/resources/Stone.tscn")
 
 
 func _on_resource_respawn_timer_timeout():
-	print("hello world")
 	var rng = RandomNumberGenerator.new()
 	var my_random_x = rng.randf_range(0.00, 325.0)
-	var my_random_y = rng.randf_range(0.0, 1300.0)
+	var my_random_y = rng.randf_range(0.0, 548.0)
 	var stone = scene.instantiate()
 	add_child(stone)
 	stone.global_position = Vector2(my_random_x, my_random_y)
-
-	print(my_random_x)
-	print(my_random_y)
