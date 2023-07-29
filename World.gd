424930b5bf24db53ad828e176feee74e35f7d6ef
Mirco Kraenz <software@kraenz.eu>
2023-07-29 10:57:34 +0200

    feat: second stone

--- World.gd
+++ World.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+var scene = preload("res://world/resources/Stone.tscn")
+
+
+func _ready():
+	var stone = scene.instantiate()
+	add_child(stone)
+	stone.global_position = Vector2(100, 100)
--- world/resources/Stone.gd
+++ world/resources/Stone.gd
@@ -5,4 +5,4 @@ extends StaticBody2D
 func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			print("A good Click!")
+			queue_free()
