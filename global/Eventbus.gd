50c707b144129fd491c9e8d25f6ba919d90901d2
Mirco Kraenz <software@kraenz.eu>
2023-07-30 12:16:48 +0200

    feat: add Wood

--- global/Eventbus.gd
+++ global/Eventbus.gd
@@ -3,3 +3,4 @@ extends Node
 signal flag_passed
 signal goal_reached
 signal stone_destroyed
+signal wood_destroyed
--- ui/hud/Inventory.gd
+++ ui/hud/Inventory.gd
@@ -3,14 +3,25 @@ extends Label
 @onready var eventbus := Eventbus
 
 var stones = 0
+var wood = 0
 
 
 func _ready():
 	eventbus.connect("stone_destroyed", _on_stone_destroyed)
+	eventbus.connect("wood_destroyed", _on_wood_destroyed)
 
 
 func _on_stone_destroyed():
 	stones = stones + 1
-	var format_string = "stone: %s"
-	var actural_string = format_string % stones
-	self.text = actural_string
+	anzeigen()
+
+
+func _on_wood_destroyed():
+	wood = wood + 1
+	anzeigen()
+
+
+func anzeigen():
+	var format_string = "stone: %s\nwood: %s"
+	var actual_string = format_string % [stones, wood]
+	self.text = actual_string
